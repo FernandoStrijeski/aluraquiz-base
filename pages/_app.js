83c81408ea2a59ba3c9a1126b32ced7ddf52d601
Fernando Ricardo <fernando.ricardo@humanus.net.br>
2021-01-27 21:44:47 -0300

    correção do globalStyle

--- pages/_app.js
+++ pages/_app.js
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider theme={theme}>
-      <GlobalStyle />
+      <GlobalStyle /> 
         <Component {...pageProps} />
       </ThemeProvider>
     </>
